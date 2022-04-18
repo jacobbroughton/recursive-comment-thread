d0b9cea71a7d6c664ab387ca9aad43347e1f08fb
jlb1999 <jlbroughton88@gmail.com>
2022-04-18 19:05:26 -0400

    Made 'er pretty with some css and added some mock json data

--- recursive-comment-thread/src/App.tsx
+++ recursive-comment-thread/src/App.tsx
@@ -1,45 +1,38 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import comments from "./comments.json";
+import users from "./users.json";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <main>
+        <div className="post">
+          <p className="posted-by">Posted by JacobBroughton 3d ago</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          </p>
+        </div>
+        <div className="comments">
+          {comments.map((comment, index) => {
+            const user = users.find((u) => u.id === comment.user_id);
+
+            return (
+              <div className="comment">
+                <div className="profile-pic-and-username">
+                  <div className="profile-pic"></div>
+                  <p className="username">
+                    {user ? user.username : `User ${comment.user_id}`}
+                  </p>
+                </div>
+                <p className='body'>{comment.body}</p>
+              </div>
+            );
+          })}
+        </div>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
